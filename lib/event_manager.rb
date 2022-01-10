9ff7855b21628cc3933cffd1e33e542670e86992
Dean <72749793+drossano@users.noreply.github.com>
2022-01-10 17:49:35 -0500

    Display attendees' representatives

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,13 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -13,7 +18,23 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  zipcode = clean_zipcode(row[:zipcode])
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting  www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
